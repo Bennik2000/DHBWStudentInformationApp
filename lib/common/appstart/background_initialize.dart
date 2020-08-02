47e389130a0237d2fa5c26db1f045fd399ca8fef
Bennik2000 <bennik.ko@gmail.com>
2020-08-02 21:41:59 +0200

    Cancel background schedule request when no rapla url is provided

--- lib/common/appstart/background_initialize.dart
+++ lib/common/appstart/background_initialize.dart
@@ -14,6 +14,7 @@ class BackgroundInitialize {
         KiwiContainer().resolve(),
         KiwiContainer().resolve(),
         KiwiContainer().resolve(),
+        KiwiContainer().resolve(),
       ),
       NextDayInformationNotification(
         KiwiContainer().resolve(),
