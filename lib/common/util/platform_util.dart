52e0672b42aae548dfe37a80915f0b9289438c65
Bennik2000 <bennik.ko@gmail.com>
2020-10-10 18:13:26 +0200

    Do not show rate in store dialog for non android platforms

--- lib/common/util/platform_util.dart
+++ lib/common/util/platform_util.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
@@ -31,5 +33,9 @@ class PlatformUtil {
         DeviceOrientation.landscapeRight,
       ]);
     }
+  }
+
+  static bool isAndroid() {
+    return Platform.isAndroid;
   }
 }
