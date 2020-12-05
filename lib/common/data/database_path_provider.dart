a64d17ec7c00e1aeefef70391cd7f9d9214be5de
Bennik2000 <bennik.ko@gmail.com>
2020-12-05 11:03:54 +0100

    Add database migration for iOS

--- lib/common/data/database_path_provider.dart
+++ lib/common/data/database_path_provider.dart
@@ -7,15 +7,51 @@ import 'package:ios_app_group/ios_app_group.dart';
 Future<String> getDatabasePath(String databaseName) async {
   Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
+  var path = join(documentsDirectory.path, databaseName);
+
   if (Platform.isIOS) {
-    documentsDirectory = await IosAppGroup.getAppGroupDirectory(
-        'group.de.bennik2000.dhbwstudentapp');
-    // TODO: Copy old database to new directory
+    path = await _getiOSDatabasePathAndMigrate(path, databaseName);
   }
 
-  final String path = join(documentsDirectory.path, databaseName);
-
-  print(path);
-
   return path;
+}
+
+Future<String> _getiOSDatabasePathAndMigrate(
+  String oldPath,
+  String databaseName,
+) async {
+  // Since the iOS widget needs access to the schedule database,
+  // it must be saved in a group shared between the app module and the widget
+  // module. "Migration" means that the database at the old path gets
+  // copied to the new path
+
+  var groupDirectory = await IosAppGroup.getAppGroupDirectory(
+    'group.de.bennik2000.dhbwstudentapp',
+  );
+
+  var newPath = join(groupDirectory.path, databaseName);
+
+  var migrateSuccess = await _migrateOldDatabase(oldPath, newPath);
+
+  if (!migrateSuccess) {
+    print("Failed to migrate database");
+  }
+
+  return newPath;
+}
+
+Future<bool> _migrateOldDatabase(String oldPath, String newPath) async {
+  try {
+    var newFile = File(newPath);
+    var oldFile = File(oldPath);
+
+    if (await oldFile.exists() && !(await newFile.exists())) {
+      oldFile.copy(newPath);
+      oldFile.delete();
+    }
+  } on Exception catch (ex) {
+    return false;
+  }
+
+  return true;
 }
