513bb5e772dca835a01366e7fee9bf9e99bec647
Bennik2000 <bennik.ko@gmail.com>
2021-05-10 22:42:21 +0200

    Publish version 1.1.7

--- lib/common/application_constants.dart
+++ lib/common/application_constants.dart
@@ -1,4 +1,4 @@
-const String ApplicationVersion = "1.1.6";
+const String ApplicationVersion = "1.1.7";
 const String ApplicationSourceCodeUrl =
     "https://github.com/Bennik2000/DHBWStudentInformationApp";
 
